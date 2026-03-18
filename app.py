dc2ad6316f16af5dd9c92824c6ae5e5b94ac580a
douwesmit11 <douwesmit11@gmail.com>
2026-03-18 12:08:22 +0100

    nieuwe paginas

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template("begin.html")
 
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+@app.route("/maakpost")
+def maakpost():
+    return render_template("maakpost.html")
+
+@app.route("/inloggen")
+def inloggen():
+    return render_template("inloggen.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

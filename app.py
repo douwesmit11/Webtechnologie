a3a6d9052b1acc6ed57d9c7fd9715927dbe46f40
douwesmit11 <douwesmit11@gmail.com>
2026-03-28 23:13:09 +0100

    Inloggen proberen te maken, begin gemaakt met de home

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from flask import session
 from flask import render_template
 from models import db, User
 import os
@@ -9,10 +10,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+app.secret_key = "geheim"
 
 @app.route("/")
 def home():
     return render_template("begin.html")
+def vorige():
+    ...
+
+
+def volgende():
+    ...
+
 
 @app.route("/account.html")
 def account():
@@ -26,8 +35,13 @@ def maakpost():
 def inloggen():
     return render_template("inloggen.html")
 
-@app.route("/regristreren.html")
+@app.route("/regristreren.html", methods=["GET", "POST"])
 def regristreren():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        return redirect("/inloggen.html")
+
     return render_template("regristreren.html")
 
 if __name__ == "__main__":
--- mijn-flask-app/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mijn-flask-app!")
-
-
-if __name__ == "__main__":
-    main()
